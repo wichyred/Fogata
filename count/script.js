ae2f6b8a8b28d5df6ee51216401c98ef2431b447
wichyred <96553289+wichyred@users.noreply.github.com>
2022-05-23 00:04:45 -0500

    Add files via upload

---
+++ count/script.js
@@ -0,0 +1,35 @@
+const $days = document.getElementById('days'),
+$hours = document.getElementById('hours'),
+$minutes = document.getElementById('minutes'),
+$seconds = document.getElementById('seconds'),
+$finalMessage = document.querySelector('.final-sms');
+
+//Fecha a futuro                MM DD  AAAA 
+const countdownDate = new Date('05 29, 2022 22:00:00').getTime();
+
+let interval = setInterval(function(){
+    //Obtener fecha actual y milisegundos
+    const now = new Date().getTime();
+
+    //Obtener las distancias entre ambas fechas
+    let distance = countdownDate - now;
+
+    //Calculos a dias-horas-minutos-segundos
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24 )) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60 )) / (1000));
+
+    //Escribimos resultados
+    $days.innerHTML = days;
+    $hours.innerHTML = hours;
+    $minutes.innerHTML = minutes;
+    $seconds.innerHTML = ('0' + seconds).slice(-2);
+
+    //Cuando llegue a 0
+    if(distance < 0){
+        clearInterval(interval);
+        // $finalMessage.style = 'translateY(0)';
+        $finalMessage.innerHTML='Comunicate a 524451540656 para que te pase el link';
+    }
+}, 1000);
